315e26eed31f39f2a90ec4512599ffe6a85e0811
edumixer <edu.cena.wf@gmail.com>
2021-02-24 16:37:17 -0300

    Criando condicionais no JSX. Finalizando o countdown. Criando contextos em ReactJS. Criando calculo para o usuario upar de level.

---
+++ moveit-next/src/contexts/ChallengesContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+/* Criando uma interface para mostrar qual tipo ela vai ter */
+interface Challenge { 
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+interface ChallengesContextData {
+  level: number;
+  currentExperiencie: number;
+  experienceToNextLevel: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  resetChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currentExperiencie, setCurrentExperiencie] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
+  
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2) /* calculo de xp para o proxiumo level */
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+  
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  return (
+    <ChallengesContext.Provider 
+      value={{ 
+        level, 
+        currentExperiencie,
+        experienceToNextLevel,
+        challengesCompleted, 
+        levelUp ,
+        startNewChallenge,
+        activeChallenge,
+        resetChallenge,
+      }}
+    >
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
